738df257a33327a957ad827311cce9cff51928c0
mikohan <angara99@gmail.com>
2021-11-10 17:32:42 +0300

    Tested sizeof operand

--- ProjectsDisk/FirstProject/main.cpp
+++ ProjectsDisk/FirstProject/main.cpp
@@ -8,5 +8,13 @@ int main() {
     int age {50}; 
     cout << age << endl;
 
+    cout << "size of information" << endl;
+    cout << "int" << sizeof(int) << endl;
+    cout << "unsigned int" << sizeof(unsigned int) << endl;
+    cout << "short" << sizeof(short) << endl;
+    cout << "long" << sizeof(long) << endl;
+    cout << "long long" << sizeof(long long) << endl;
+
+
     return 0;
 }
